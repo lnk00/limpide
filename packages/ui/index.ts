39e716b59feaf13e73535814d9f3f71df3261703
Damien Dumontet <damien@fullenrich.com>
2025-09-05 08:43:52 -0300

    modify biome conf

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,3 @@
-export { Counter } from "./components/counter";
-export { Header } from "./components/header";
-export { setupCounter } from "./utils/counter";
+export { Counter } from './components/counter';
+export { Header } from './components/header';
+export { setupCounter } from './utils/counter';
